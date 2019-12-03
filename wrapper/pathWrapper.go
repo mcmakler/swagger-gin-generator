03a36f8d96a636d1af6f465efd4e9e0a8736487e
Nikita Kharitonov <nikita.kharitonov@mcmakler.de>
2019-12-03 12:32:34 +0100

    feat(mainSwagg): add check version and title

--- wrapper/pathWrapper.go
+++ wrapper/pathWrapper.go
@@ -10,21 +10,13 @@ import (
 
 type SwaggPathWrapper interface {
 	Get(
-		description string,
-		consumes []string,
-		produces []string,
-		tags []string,
-		summary string,
+		swaggerParameters map[string]interface{},
 		parameters []utils.Parameter, //TODO: think
 		requests map[int]Request,
 		handlerFunc ...gin.HandlerFunc,
 	)
 	Post(
-		description string,
-		consumes []string,
-		produces []string,
-		tags []string,
-		summary string,
+		swaggerParameters map[string]interface{},
 		parameters []utils.Parameter, //TODO: think
 		requests map[int]Request,
 		handlerFunc ...gin.HandlerFunc,
@@ -58,11 +50,7 @@ func NewSwaggPathWrapper(path, tag string, group *gin.RouterGroup) SwaggPathWrap
 }
 
 func (s *swaggPathWrapper) Get(
-	descriptionP string,
-	consumesP []string,
-	producesP []string,
-	tagsP []string,
-	summaryP string,
+	swaggerParameters map[string]interface{},
 	parametersP []utils.Parameter,
 	requests map[int]Request,
 	handlerFuncP ...gin.HandlerFunc,
@@ -88,32 +76,21 @@ func (s *swaggPathWrapper) Get(
 	}
 
 	if s.tag != "" {
-		if tagsP == nil {
-			tagsP = []string{}
+		if _, ok := swaggerParameters["tagsP"]; !ok {
+			swaggerParameters["tagsP"] = []string{}
 		}
-		tagsP = append(tagsP, s.tag)
+		swaggerParameters["tagsP"] = append(swaggerParameters["tagsP"].([]string), s.tag)
 	}
 
 	s.requests = append(s.requests, swaggerFileGenerator.NewRequestSwagg(
-		map[string]interface{}{
-			"typeRequest": "get",
-			"description": descriptionP,
-			"summary":     summaryP,
-			"consumes":    consumesP,
-			"produces":    producesP,
-			"tags":        tagsP,
-		},
+		swaggerParameters,
 		paramsSwagg,
 		responses,
 	))
 }
 
 func (s *swaggPathWrapper) Post(
-	descriptionP string,
-	consumesP []string,
-	producesP []string,
-	tagsP []string,
-	summaryP string,
+	swaggerParameters map[string]interface{},
 	parametersP []utils.Parameter,
 	requests map[int]Request,
 	handlerFuncP ...gin.HandlerFunc,
@@ -139,21 +116,14 @@ func (s *swaggPathWrapper) Post(
 	}
 
 	if s.tag != "" {
-		if tagsP == nil {
-			tagsP = []string{}
+		if _, ok := swaggerParameters["tagsP"]; !ok {
+			swaggerParameters["tagsP"] = []string{}
 		}
-		tagsP = append(tagsP, s.tag)
+		swaggerParameters["tagsP"] = append(swaggerParameters["tagsP"].([]string), s.tag)
 	}
 
 	s.requests = append(s.requests, swaggerFileGenerator.NewRequestSwagg(
-		map[string]interface{}{
-			"typeRequest": "post",
-			"description": descriptionP,
-			"summary":     summaryP,
-			"consumes":    consumesP,
-			"produces":    producesP,
-			"tags":        tagsP,
-		},
+		swaggerParameters,
 		paramsSwagg,
 		responses,
 	))
--- wrapper/routerWrapper_test.go
+++ wrapper/routerWrapper_test.go
@@ -37,11 +37,13 @@ func TestNewSwaggerRouterWrapper(t *testing.T) {
 		firstGroup := srw.Group("/group1", "firstGroup")
 		path1 := firstGroup.Path("/path1")
 		path1.Get(
-			"getDescription",
-			[]string{"getConsume"},
-			[]string{"getProduce"},
-			nil,
-			"getSummary",
+			map[string]interface{}{
+				"description": "getDescription",
+				"consumes": []string{"getConsume"},
+				"produces": []string{"getProduce"},
+				"tags": []string{"tagget"},
+				"summary": "getSummary",
+			},
 			[]utils.Parameter{
 				utils.NewParameter(map[string]interface{}{
 					"name":        "name",
@@ -68,11 +70,13 @@ func TestNewSwaggerRouterWrapper(t *testing.T) {
 			},
 			func(c *gin.Context) {})
 		path1.Post(
-			"getDescription",
-			[]string{"getConsume"},
-			[]string{"getProduce"},
-			nil,
-			"getSummary",
+			map[string]interface{}{
+				"description": "getDescription",
+				"consumes": []string{"getConsume"},
+				"produces": []string{"getProduce"},
+				"tags": []string{"tagget"},
+				"summary": "getSummary",
+			},
 			[]utils.Parameter{
 				utils.NewParameter(map[string]interface{}{
 					"name":        "name",
@@ -90,11 +94,7 @@ func TestNewSwaggerRouterWrapper(t *testing.T) {
 
 		path2 := firstGroup.Path("/path2")
 		path2.Get(
-			"getDescription",
-			[]string{"getConsume"},
-			[]string{"getProduce"},
 			nil,
-			"getSummary",
 			[]utils.Parameter{
 				utils.NewParameter(map[string]interface{}{
 					"name":        "name",
@@ -110,11 +110,7 @@ func TestNewSwaggerRouterWrapper(t *testing.T) {
 			},
 			func(c *gin.Context) {})
 		path2.Post(
-			"getDescription",
-			[]string{"getConsume"},
-			[]string{"getProduce"},
 			nil,
-			"getSummary",
 			[]utils.Parameter{
 				utils.NewParameter(map[string]interface{}{
 					"name": "name",
@@ -131,11 +127,13 @@ func TestNewSwaggerRouterWrapper(t *testing.T) {
 		secondGroup := srw.Group("/group2", "secondGroup")
 		paht22 := secondGroup.Path("/path2")
 		paht22.Get(
-			"getDescription",
-			[]string{"getConsume"},
-			[]string{"getProduce"},
-			nil,
-			"getSummary",
+			map[string]interface{}{
+				"description": "getDescription",
+				"consumes": []string{"getConsume"},
+				"produces": []string{"getProduce"},
+				"tags": []string{"tagget"},
+				"summary": "getSummary",
+			},
 			[]utils.Parameter{
 				utils.NewParameter(map[string]interface{}{
 					"name": "name",
