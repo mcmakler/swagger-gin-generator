e74cb6ab2e2bb8b606ae0bcaee847bdcb17bcece
Nikita Kharitonov <nikita.kharitonov@mcmakler.de>
2019-12-09 16:14:28 +0100

    fix(wrapper): fixing 404

--- wrapper/groupWrapper.go
+++ wrapper/groupWrapper.go
@@ -45,6 +45,7 @@ func (s *swaggerGroupWrapper) Path(path string) SwaggerPathWrapper {
 
 func (s *swaggerGroupWrapper) Group(path, tag string) SwaggerGroupWrapper {
 	group := s.group.Group(s.path + path)
+	fmt.Println(s.path + " " + path)
 	fmt.Println(group)
 	res := newSwaggerGroupWrapper(s.path+path, tag, group)
 	s.subgroups = append(s.subgroups, res)
